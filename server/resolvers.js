c8f0272fbc450bda5ebedc2538c5e8a59e022dbf
LuthfiFRahman <luthfifrahman@economist.com>
2024-12-16 21:53:38 +0700

    finish Section 3: Field Resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,19 @@
+import { getJobs } from './db/jobs.js';
+
 export const resolvers = {
     Query: {
-      greetings: () => 'hello world',
+        jobs: async () => {
+            return await getJobs();
+        },
     },
-  };
-  
+
+    Job: {
+        // case: field names of date and time are not the same on DB and GraphQL schema. resolving it as follow:
+        date: (job) =>  toIsoDate(job.createdAt),
+    },
+};
+
+// showing date only
+function toIsoDate(value) {
+    return value.slice(0, 'yyyy-mm-dd'.length);
+}
